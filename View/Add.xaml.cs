ddb9b413ac3d4e0e79eca2d5be8375c7efb0de81
Kolovratik <Kolovratik@HOME-PC>
2024-06-11 20:34:54 +0300

    Созданная страница добавления и изменения

---
+++ View/Add.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PR42.ViewModell;
+using System.Windows.Controls;
+
+namespace PR42.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        public Add(object Context)
+        {
+            InitializeComponent();
+            DataContext = new
+            {
+                item = Context,,
+                categories = new VMCategories()
+            };
+        }
+    }
+}
