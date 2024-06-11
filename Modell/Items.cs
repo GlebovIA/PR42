884e9c464a23c2d8f9cb5cf2e9d60461eb7631f7
Kolovratik <Kolovratik@HOME-PC>
2024-06-11 19:41:52 +0300

    Разработанные модели данных

---
+++ Modell/Items.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PR42.Modell
+{
+    public class Items : INotifyPropertyChanged
+    {
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        private double _price;
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+        private Categories _category;
+        public double Category
+        {
+            get { return _category; }
+            set
+            {
+                _category = value;
+                OnPropertyChanged("Category");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
