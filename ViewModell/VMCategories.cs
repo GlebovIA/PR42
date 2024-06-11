1b52666993c5fd68678a4beae26ab735376781d0
Kolovratik <Kolovratik@HOME-PC>
2024-06-11 20:20:25 +0300

    Разработанные модели представления

---
+++ ViewModell/VMCategories.cs
@@ -0,0 +1,18 @@
+﻿using PR42.Context;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PR42.ViewModell
+{
+    public class VMCategories : INotifyPropertyChanged
+    {
+        public ObservableCollection<CategoriesContext> Categories { get; set; }
+        public VMCategories() => Categories = CategoriesContext.AllCategories();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ViewModell/VMItems.cs
@@ -0,0 +1,30 @@
+﻿using PR42.Context;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PR42.ViewModell
+{
+    public class VMItems : INotifyPropertyChanged
+    {
+        public ObservableCollection<Context.ItemsContext> Items { get; set; }
+        public Classes.RelayCommand NewItem
+        {
+            get
+            {
+                return new Classes.RelayCommand(obj =>
+                {
+                    Context.ItemsContext newModell = new Context.ItemsContext(true);
+                    Items.Add(newModell);
+                    MainWindow.init.frame.Navigate(new View.Add(newModell));
+                });
+            }
+        }
+        public VMItems() => Items = ItemsContext.AllItems();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
