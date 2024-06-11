9d60e6fb84aa099f81ea707491217dbda1bd2139
Kolovratik <Kolovratik@HOME-PC>
2024-06-11 19:31:36 +0300

    Разработанные классы общего пользования

---
+++ Classes/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+
+namespace PR42.Classes
+{
+    public class Connection
+    {
+        private static readonly string config = "" +
+            "Trusted_Connection=No;" +
+            "DataBase=ShopContent;" +
+            "User=;" +
+            "PWD=";
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+        public static SqlDataReader Query(string sql, out SqlConnection connection)
+        {
+            connection = OpenConnection();
+            return new SqlCommand(sql, connection).ExecuteReader();
+        }
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
